65ae792991f5983ed35b11f24af17cd02eb3eff1
GreenSalat <29101999tima@gmail.com>
2020-11-20 15:23:30 +0300

    add route(2)

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,4 +9,4 @@ def test_index(app, client):
 def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
-    assert res.data == b"Second action"
+    assert res.data == b"Hello from auto apdate"
